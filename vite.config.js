abeb714184cf3397a2efd284be6e8c3aabea3114
Nhung Péo <tho@Nhung-MacBook-Pro.local>
2024-12-19 01:57:21 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/web-tieu-luan/"
+  base: "/web-tieu-luan-1"
 });
